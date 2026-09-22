a4f5a2efe190d6146c3a7d8f705285add349d70f
elenafff <fangy5552@gmail.com>
2026-09-22 15:30:27 -0400

    class6

---
+++ class-demos/class 6 demo/main.js
@@ -0,0 +1,12 @@
+// this is a comment
+// syntax is //
+
+alert("Javascript!")
+console.log("log this information to the console")
+
+
+// shorthand for waiting for the whole webpage to load
+// could be useful when multiple images/large files embeded
+window.onload = () => {
+    console.log("page has loaded")
+}
